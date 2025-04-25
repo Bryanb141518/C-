7346419cd43d4bef0e7b8390b714b58a70113cb0
brayan benitez <bryan141518@gmail.com>
2025-04-25 09:51:10 -0500

    primer commit en c#

---
+++ primeros_pasos_C#/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class hola
+{
+    static void Main(String[] args)
+    {
+        
+
+        Console.WriteLine("introduce la medida del radio");
+        double radio = double.Parse(Console.ReadLine());
+        double area = Math.Sqrt(radio); 
+        Console.WriteLine($"el area del circulo es {area}");
+    }
+}     
