20dd100ebfcdcbc3c6f3fa768f9d79c5d3d58461
brayan benitez <bryan141518@gmail.com>
2025-04-28 11:31:54 -0500

    Añadir archivo condicionales.cs

---
+++ primeros_pasos_C#/condicionales.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CondicionalIF
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            bool haceFrio;
+
+            haceFrio = true;
+
+            Console.WriteLine(haceFrio);
+        }
+    }
+} 
